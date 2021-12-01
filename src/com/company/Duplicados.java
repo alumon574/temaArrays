ece8eaa6c3492ff0abc9d1807318a6d0f1956c1b
alumon574 <alumon574@ieselcaminas.org>
2021-12-01 19:13:39 +0100

    duplicados hecho

--- src/com/company/Duplicados.java
+++ src/com/company/Duplicados.java
@@ -1,11 +1,22 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duplicados {
     public static void main(String[] args) {
-        Scanner reader=new Scanner(System.in);
-        int[] listaNums=new int[10];
-        
+        Scanner reader = new Scanner(System.in);
+        int[] listaNums = new int[10];
+        String numsIguales = "";
+        Utilidades.rellenaArray(listaNums, 0, 10);
+        System.out.println(Arrays.toString(listaNums));
+        for (int i = 0; i < listaNums.length; i++) {
+            for (int j = i + 1; j < listaNums.length; j++) {
+                if (listaNums[i] == listaNums[j]) {
+                    numsIguales = numsIguales + "|" + listaNums[j];
+                    System.out.println(numsIguales + "|");
+                }
+            }
+        }
     }
 }
